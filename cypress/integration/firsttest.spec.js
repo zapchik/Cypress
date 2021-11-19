de7b60ca2f01f9d682473b00582f27d35c5e4ef8
zapchik <foramazon157@gmail.com>
2021-11-19 07:01:35 +0200

    Solution2

--- cypress/integration/firsttest.spec.js
+++ cypress/integration/firsttest.spec.js
@@ -2,10 +2,14 @@
 describe('Conduit test', () => {
     it('Conduit log in', () => {
         cy.visit('https://cirosantilli-realworld-next.herokuapp.com/');
-        cy.get(':nth-child(2) > .nav-link').click();
-        cy.get(':nth-child(1) > .form-control').type('zapchik@meta.ua');
-        cy.get(':nth-child(2) > .form-control').type('robfish1965');
+        cy.get(':nth-child(2) > .nav-link')
+        .click();
+        cy.get(':nth-child(1) > .form-control')
+        .type('zapchik@meta.ua');
+        cy.get(':nth-child(2) > .form-control')
+        .type('robfish1965');
         cy.get('.btn').click();
-        cy.get(':nth-child(4) > .nav-link').should('contain','rob1965');
+        cy.get(':nth-child(4) > .nav-link')
+        .should('contain','rob1965');
     });
 });
