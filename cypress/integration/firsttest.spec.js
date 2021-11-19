30045b75fd4ef696571fac4d6eed094c249c32a9
zapchik <foramazon157@gmail.com>
2021-11-19 06:50:11 +0200

    Solution

---
+++ cypress/integration/firsttest.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+describe('example to-do app', () => {
+    it('', () => {
+        cy.visit('https://cirosantilli-realworld-next.herokuapp.com/');
+        cy.get(':nth-child(2) > .nav-link').click();
+        cy.get(':nth-child(1) > .form-control').type('zapchik@meta.ua');
+        cy.get(':nth-child(2) > .form-control').type('robfish1965');
+        cy.get('.btn').click();
+        cy.get(':nth-child(4) > .nav-link').should('contain','rob1965');
+    });
+});
